77d3719f22edb01ed57032390fbe7ffec9f67f9d
Edwin5658 <edwinchiu618@gmail.com>
2025-12-18 14:29:46 +0800

    minor fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';  
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
     PrismaModule,
+    UserModule,
     PostModule
   ]
 })
---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,23 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User as UserModel } from '../../generated/prisma/client';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/create')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
