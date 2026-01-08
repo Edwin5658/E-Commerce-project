6e43c1fe42df856ffecdf609ca85ff1b7971cde3
Edwin5658 <edwinchiu618@gmail.com>
2026-01-08 11:31:55 +0800

    chore: set up nextjs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { StrictMode } from "react";
+import "./index.css";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import ReactDOM from "react-dom/client";
 
 const router = createRouter({
   routeTree,
@@ -12,14 +12,19 @@ const router = createRouter({
   defaultStructuralSharing: true,
 });
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root")!;
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
